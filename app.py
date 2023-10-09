a46061c616293c85dc6fa6c26f452704483f5958
CodigyX <105528912+CodigyX@users.noreply.github.com>
2023-10-09 23:41:42 +0000

    update

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import numpy as np
 from joblib import load
 import pathlib
 
-app = FastAPI(title = 'Heart Disease Prediction')
+app = FastAPI(title= 'Heart Disease Classifier')
 
 model = load(pathlib.Path('model/heart-disease-v1.joblib'))
 
 class InputData(BaseModel):
     age:int=64
-    sex:int=1
+    sex:int=1 
     cp:int=3
     trestbps:int=120
     chol:int=267
@@ -22,13 +23,22 @@ class InputData(BaseModel):
     slope:int=1
     ca:int=2
     thal:int=2
-
+    
 class OutputData(BaseModel):
     score:float=0.80318881046519
+    
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"])
+    
 @app.post('/score', response_model = OutputData)
 def score(data:InputData):
     model_input = np.array([v for k,v in data.dict().items()]).reshape(1,-1)
     result = model.predict_proba(model_input)[:,-1]
-
+    
     return {'score':result}
